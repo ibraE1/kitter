94acdb369a076a3f7c13c34dac5f7451f4bac4f5
ibraE1 <88250111+ibraE1@users.noreply.github.com>
2023-01-29 14:56:41 +0200

    Setup dotenv and mongodb connection

--- api/app.js
+++ api/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
+config();
 var app = express();
 const port = 5000;
 
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Connected to MongoDB");
+  app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}`);
+  });
 });
