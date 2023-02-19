c6c0372dd4aec416877955784096bb076de804ce
ibraE1 <88250111+ibraE1@users.noreply.github.com>
2023-02-19 19:52:13 +0200

    Changed textarea styling based on length

--- client/src/components/Compose.jsx
+++ client/src/components/Compose.jsx
@@ -7,28 +7,48 @@ function Compose() {
   const handleSubmit = (e) => {
     e.preventDefault();
     const post = async () => {
-      await createPost(postContent);
+      await createPost(postContent.trim());
     };
     post();
     setPostContent("");
   };
 
+  const handleChange = (e) => {
+    setPostContent(e.target.value);
+  };
+
   return (
-    <div className="w-screen">
-      <form className="flex flex-col justify-center gap-4 items-center h-screen p-8">
+    <div className="w-screen h-screen">
+      <form className="flex flex-col justify-around gap-4 items-center h-[calc(100vh-4rem)] p-8">
         <textarea
-          className="w-full h-4/6 resize-none border-2 border-slate-400 placeholder:text-center placeholder-slate-400 focus:border-indigo-700 focus:placeholder-indigo-700 focus:outline-none focus:placeholder-opacity-0 rounded-lg p-3"
+          className={
+            postContent.length <= 140
+              ? "w-full h-4/5 resize-none border-2 border-slate-400 placeholder:text-center placeholder-slate-400 focus:border-indigo-700 focus:placeholder-indigo-700 focus:outline-none focus:placeholder-opacity-0 rounded-lg p-3"
+              : "w-full h-4/5 resize-none border-2 border-red-500 placeholder:text-center placeholder-slate-400 focus:border-red-600 focus:outline-none focus:placeholder-opacity-0 rounded-lg p-3"
+          }
           id="text"
           value={postContent}
           placeholder="What would you like to share?"
-          onChange={(e) => setPostContent(e.target.value)}
+          onChange={handleChange}
         ></textarea>
-        <button
-          className="bg-indigo-700 hover:bg-indigo-500 text-white font-bold font-display rounded-full w-2/5 p-4"
-          onClick={handleSubmit}
-        >
-          Post
-        </button>
+        <div className="w-full flex justify-between items-center p-2">
+          <p
+            className={
+              postContent.length > 140
+                ? "font-display font-medium text-red-600"
+                : "font-display font-medium"
+            }
+          >
+            {postContent.length}/140
+          </p>
+          <button
+            className="bg-indigo-700 hover:bg-indigo-500 text-white font-bold font-display rounded-full w-2/5 p-4 disabled:bg-indigo-300"
+            onClick={handleSubmit}
+            disabled={postContent.length > 140 || postContent.trim() == ""}
+          >
+            Post
+          </button>
+        </div>
       </form>
     </div>
   );
