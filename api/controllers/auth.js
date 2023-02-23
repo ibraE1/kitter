6aa804ecbccac861f9ece4eaa873b17dcbbfbe9e
ibraE1 <88250111+ibraE1@users.noreply.github.com>
2023-02-23 18:04:32 +0200

    Updated auth controller and middleware

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,54 +3,90 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const register = async (req, res) => {
-  const hashedPassword = await bcrypt.hash(await req.body.password, 10);
-  const newUser = new User({
-    username: req.body.username,
-    password: hashedPassword,
-    displayName: req.body.displayName || req.body.username,
-  });
   try {
+    if (!req.body.username) {
+      return res.status(400).json("Username cannot be empty");
+    }
+    if (req.body.username.indexOf(" ") >= 0) {
+      return res.status(400).json("Username cannot contain spaces");
+    }
+    if (req.body.username.length < 4 || req.body.username.length > 15) {
+      return res.status(400).json("Username must be 4-15 characters");
+    }
+
+    if (!req.body.password) {
+      return res.status(400).json("Password cannot be empty");
+    }
+    if (req.body.password.indexOf(" ") >= 0) {
+      return res.status(400).json("Password cannot contain spaces");
+    }
+    if (req.body.password.length < 8) {
+      return res.status(400).json("Password must be at least 8 characters");
+    }
+
+    let displayName = req.body.displayName || req.body.username;
+    displayName = displayName.trim();
+    if (displayName.length > 20) {
+      return res
+        .status(400)
+        .json("Display name must be 20 characters or fewer");
+    }
+
+    const existingUser = await User.findOne({ username: req.body.username });
+    if (existingUser) {
+      return res.status(400).json("Username is already taken");
+    }
+
+    const hashedPassword = await bcrypt.hash(await req.body.password, 10);
+    const newUser = new User({
+      username: req.body.username,
+      password: hashedPassword,
+      displayName: displayName,
+    });
+
     const user = await newUser.save();
-    user.password = null;
-    res.json(user.id);
+    return res.status(200).json(user.id);
   } catch (error) {
-    res.json(error);
+    return res.status(500).json(error.message);
   }
 };
 
 const login = async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if (!user) return res.json("Username is not registered");
+    if (!user) return res.status(400).json("Username is not registered");
+
     if (await bcrypt.compare(req.body.password, user.password)) {
       const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
         expiresIn: "24h",
       });
+
       res.cookie("accessToken", token, {
         maxAge: 24 * 60 * 60 * 1000, //24 hours
         httpOnly: true,
       });
-      res.json(user.id);
-    } else res.json("Wrong password");
+
+      return res.status(200).json(user.id);
+    } else res.status(400).json("Wrong password");
   } catch (error) {
-    res.json(error);
+    return res.status(500).json(error.message);
   }
 };
 
 const logout = async (req, res) => {
   try {
     res.clearCookie("accessToken", { httpOnly: true });
-    res.json("Logged Out");
+    return res.status(200).json("Logged Out");
   } catch (error) {
-    res.json(error);
+    return res.status(500).json(error.message);
   }
 };
 
 const verifyLogin = async (req, res) => {
   try {
-    res.json({ userId: req.userid, status: "Authorized" });
+    return res.status(200).json(req.userid);
   } catch (error) {
-    res.json(error);
+    return res.status(500).json(error.message);
   }
 };
 
--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -2,19 +2,19 @@ import jwt from "jsonwebtoken";
 
 const verifyToken = async (req, res, next) => {
   const cookie = req.headers.cookie;
-  if (!cookie) return res.json("Not authorized");
+  if (!cookie) return res.status(400).json("Not authorized");
 
   const token = cookie.slice(12);
   try {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-      if (err) return res.json("Invalid token");
+      if (err) return res.status(400).json("Invalid token");
       else {
         req.userid = decodedToken.id;
         return next();
       }
     });
   } catch (error) {
-    res.json(error);
+    res.status(500).json(error);
   }
 };
 
